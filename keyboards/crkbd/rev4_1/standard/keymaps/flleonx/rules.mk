f5ca97e78fb98434cc808034e3db5ebe972c4b17
flleonx <flleondev@gmail.com>
2025-03-25 08:53:54 +0100

    Refactor layout

---
+++ keyboards/crkbd/rev4_1/standard/keymaps/flleonx/rules.mk
@@ -0,0 +1,13 @@
+# MCU settings for CRKBD
+MCU = RP2040  # Assuming rev4_1 uses RP2040; adjust if different
+BOOTLOADER = rp2040
+
+# Split keyboard settings
+SPLIT_KEYBOARD = yes
+
+# Disable RGB Matrix
+RGB_MATRIX_ENABLE = no
+
+# Optional: Enable other features if needed
+OLED_ENABLE = no  # If your CRKBD has OLEDs
+ENCODER_ENABLE = no  # Adjust based on your hardware
--- users/manna-harbour_miryoku/custom_rules.mk
+++
@@ -1,7 +0,0 @@
-# Copyright 2019 Manna Harbour
-# https://github.com/manna-harbour/miryoku
-
-MIRYOKU_ALPHAS=QWERTY
-MIRYOKU_EXTRA=COLEMAKDH
-MIRYOKU_CLIPBOARD=MAC
-MIRYOKU_NAV=VI
