3bfecb6a9da4981c571d3b33cca2abe299c69cfa
flleonx <flleondev@gmail.com>
2025-04-08 09:12:45 +0200

    Change symbols row

--- keyboards/crkbd/rev4_1/standard/keymaps/flleonx/keymap.c
+++ keyboards/crkbd/rev4_1/standard/keymaps/flleonx/keymap.c
@@ -13,17 +13,17 @@ enum layers {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_DEFAULT] = LAYOUT_split_3x6_3_ex2(
-        KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_NO,    KC_NO,    KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_NO,
-        KC_LCTL,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_NO,    KC_RALT,  KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
+        KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_NO,    KC_NO,    KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_RGUI,
+        KC_LCTL,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     LGUI(KC_GRV),    KC_RALT,  KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
         KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,
                                       LOWER,  LALT_T(KC_ESC),    LGUI_T(KC_ENTER),           RAISE,          KC_BSPC,  LOWER
     ),
 
     [_LOWER] = LAYOUT_split_3x6_3_ex2(
         KC_TRNS,  KC_EXLM,  KC_AT,    KC_HASH,  KC_DLR,   KC_PERC,  KC_NO,    KC_NO,    KC_CIRC,  KC_AMPR,  KC_ASTR,  LSFT(KC_9), LSFT(KC_0), KC_BSLS,
-        KC_TRNS,  KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_NO,    KC_RALT,  KC_MINS,  KC_EQL,   KC_GRV,   KC_LBRC,  KC_RBRC,    KC_PIPE,
-        KC_LSFT,  KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_UNDS,  KC_PLUS,  KC_TILD,  KC_LCBR,  KC_RCBR,  KC_RSFT,
-                                      KC_LGUI,  LALT_T(KC_ESC),  LGUI_T(KC_ENTER),           RAISE,          KC_BSPC,  KC_TRNS
+        KC_TRNS,  KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_NO,    KC_RALT,  KC_6,     KC_7,     KC_8,     KC_9,     KC_0,    KC_PIPE,
+        KC_LSFT,  KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,     KC_MINS,  KC_EQL,   KC_GRV,   KC_LBRC,  KC_RBRC,  KC_RSFT,
+                                      KC_TRNS,  LALT_T(KC_ESC),  LGUI_T(KC_ENTER),           RAISE,          KC_BSPC,  KC_TRNS
     ),
 
     [_RAISE] = LAYOUT_split_3x6_3_ex2(
